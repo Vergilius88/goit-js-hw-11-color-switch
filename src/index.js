9919731b3e1bc8d5a09489daebb5b7bd33c980e2
Nikolaienko Ihor <nikolasha8876@gmail.com>
2020-10-21 21:16:48 +0300

    fix gaps

--- src/index.js
+++ src/index.js
@@ -12,30 +12,24 @@ const colors = [
   '#009688',
   '#795548',
 ];
-
 const min = 0;
 const max = colors.length - 1;
 let intervalChangeColors;
-
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
 const changeColor = () => {
   ref.body.style.backgroundColor = colors[randomIntegerFromInterval(min, max)];
 };
-
 const letStart = () => {
   ref.btnStart.setAttribute('disabled', 'disabled');
   intervalChangeColors = setInterval(() => {
     changeColor();
   }, 1000);
 };
-
 const letStop = () => {
   clearInterval(intervalChangeColors);
   ref.btnStart.removeAttribute('disabled');
 };
-
 ref.btnStart.addEventListener('click', letStart);
 ref.btnStop.addEventListener('click', letStop);
